74aa03bcb8eaad5f507a8e700403773aa859f6e7
Agustin <rojasagustin90@gmail.com>
2016-07-01 20:17:10 -0300

    gradle ok

--- tp/src/main/java/fallas/Tp.java
+++ tp/src/main/java/fallas/Tp.java
@@ -1,6 +1,11 @@
+package fallas;
 /**
  * Created by agu on 25/06/16.
  */
+import org.drools.RuleBase;
+import org.drools.WorkingMemory;
+import org.drools.compiler.DroolsParserException;
+
 public class Tp {
     public static void main(String[] args) {
         System.out.print("Hola");
